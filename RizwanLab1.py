a3e2b1988296c350bd346eed904f88511acac469
Muhammad Rizwan <ahmadzainab@outlook.com>
2024-09-16 23:26:45 -0400

    Add files via upload

---
+++ RizwanLab1.py
@@ -0,0 +1,23 @@
+# Saying my full name
+print('   ')
+print('   Hello! My name is Muhammad Ahmad Rizwan.                                                                                  #');
+print('                                                                                                                             #');
+# Explaining where I'm from originally and my major and minor that I am doing. 
+print('   I was born in Lahore, Pakistan and I moved to Indiana when I was about 3 years old.                                       #');
+print('                                                                                                                             #');
+print('   My major is Computer Science, and I am also pursuing a Business minor alongside an A.I. certification.                    #');
+print('                                                                                                                             #########');
+ #');
+# Listing my various hobbies that I like to do in my free time
+print('   I like to do a lot of different things in my free time, these are my favorite things to do:');
+print('   ');
+print('         > Basketball');
+print('         > Soccer');
+print('         > Video Games');
+print('         > Hanging out with friends');
+print('  ');
+
+
+
+
+
